f7a5bce7d8d9c35af2f8d107f233b442904ca042
Ricardo Garcia <ricardo@pixe.com.mx>
2013-04-10 12:16:43 -0500

    VM! Lee archivo y ejecuta saltos

--- vm/phast_vm.php
+++ vm/phast_vm.php
@@ -7,12 +7,68 @@ if(!is_file($file))
     die("Phast:\n Could not read file => $file\n");
 
 $file_handle = fopen($file, "r+");
-if($file_handle){
-while (!feof($file_handle)) {
-   $line = fgets($file_handle);
-   echo $line;
-}
-fclose($file_handle);
-}else
+if(!$file_handle){
     echo "Error reading program.\n";
+    die();
+}else{
+    $source = array();
+    $EOF = 0;
+    while ($line = fgets($file_handle)) {
+        $source[] = split("\t",str_replace("\n","",$line));
+        $EOF++;
+    }
+    fclose($file_handle);
+
+    $curr_reg = 0;
+
+    while($curr_reg < $EOF)
+    {
+        $instruccion = $source[$curr_reg];
+        switch($instruccion[0])
+        {
+            #Flow Control instructions
+        case "GotoF":
+            echo "GOTOF ${instruccion[3]}\n";
+            $curr_reg++; //TODO Quitar saltos
+                break;
+        case "GotoV":
+            echo "GOTOV ${instruccion[3]}\n";
+            $curr_reg++; //TODO Quitar saltos
+                break;
+        case "Goto":
+            // $curr_reg = (int)$instruccion[3];
+            $curr_reg++; //TODO Quitar saltos
+            echo "GOTO ${instruccion[3]}\n";
+                break;
+            #Arithmetic instructions
+        case "+":
+            echo "SUM ${instruccion[1]} ${instruccion[2]}\n";
+            $curr_reg++;
+                break;
+        case "*":
+            echo "MUL ${instruccion[1]} ${instruccion[2]}\n";
+            $curr_reg++;
+                break;
+            #Logical instructions
+        case "=":
+            echo "ASIGN ${instruccion[1]} ${instruccion[2]}\n";
+            $curr_reg++;
+                break;
+        case ">":
+            echo "GT ${instruccion[1]} ${instruccion[2]}\n";
+            $curr_reg++;
+                break;
+        case "<":
+            echo "LT ${instruccion[1]} ${instruccion[2]}\n";
+            $curr_reg++;
+                break;
+        default:
+            echo $curr_reg;
+            // echo "Unknown instruction:\n".implode("\t",$instruccion);
+            die();
+        }
+
+    }
+
+}
 ?> 
